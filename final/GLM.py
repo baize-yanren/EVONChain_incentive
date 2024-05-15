71ae5dca9c43fe91f546e5f25c060825210c9416
baize-yanren <1062973115@qq.com>
2024-05-15 15:01:03 +0800

    经济声誉混合模拟完善

--- final/GLM.py
+++ final/GLM.py
@@ -1,7 +1,21 @@
+# -*- encoding: utf-8 -*-
+'''
+@Description    :经济声誉混合模拟
+@Date           :2024/05/15 14:34:33
+@Author         :WangYR
+@version        :1.0
+'''
+
+import random
+
+# 补充的类和函数
+
 class Transaction:
-    def __init__(self, amount, size, fee):
+    def __init__(self, sender, recipient, size, amount, fee):
+        self.sender = sender  # 发送者
+        self.recipient = recipient  # 接收者
+        self.size = size # 交易大小
         self.amount = amount  # 交易金额
-        self.size = size  # 交易大小
         self.fee = fee  # 交易费
 
 class CloudNode:
@@ -68,15 +82,15 @@ class UserNode:
     def __init__(self):
         self.wallet = 1000  # 初始钱包金额
 
-    def send_transaction(self, recipient, amount, fee):
+    def send_transaction(self, transaction, amount, fee):
         """
         发起交易
         """
         if self.wallet >= amount + fee:
             self.wallet -= amount + fee
-            recipient.receive_transaction(amount, fee)
+            transaction.recipient.receive_transaction(amount)
 
-    def receive_transaction(self, amount, fee):
+    def receive_transaction(self, amount):
         """
         接收交易
         """
@@ -85,27 +99,88 @@ class UserNode:
     def __str__(self):
         return f"User Node - Wallet: {self.wallet}"
 
+def generate_nodes(user_count, cloud_count, miner_count, beta):
+    """
+    生成指定数量的用户节点、云节点和矿工节点
+    """
+    users = [UserNode() for _ in range(user_count)]
+    clouds = [CloudNode() for _ in range(cloud_count)]
+    miners = [MinerNode(1, 1, beta) for _ in range(miner_count)]
+    return users, clouds, miners
 
+def generate_transactions(users):
+    """
+    用户节点之间随机生成交易
+    """
+    transaction_pool = []
+    for _ in range(100):
+        sender = random.choice(users)
+        recipient = random.choice(users)
+        while recipient == sender:
+            recipient = random.choice(users)
+        amount = random.randint(1, 100)
+        fee = random.uniform(0, 10)
+        size=random.uniform(1000,20000)#1kb-20kb
+        transaction = Transaction(sender, recipient, size, amount, fee)
+        sender.send_transaction(transaction,amount, fee)
+        transaction_pool.append(transaction)
+    return transaction_pool
 
+def pack_and_mine(clouds, miners, transaction_pool):
+    """
+    云节点进行一次打包，矿工节点进行挖矿和验证
+    """
+    # 云节点选择最有利的交易进行打包
+    packer = random.choice(clouds)
+    miner = random.choice(miners)
+    sum_size = 0
+    limit_size = 1000000
+    num=random.randint(10,1000)
+    i=0
+    while sum_size<limit_size or i==num:
+        packer.pack(transaction_pool, random.choice(miners))
+        i+=1
 
-# 模拟交易池
-transaction_pool = []
+    # 矿工节点进行挖矿和验证
+    return miner.validate(packer, transaction_pool)
 
-# 创建节点
-cloud_node = CloudNode()
-miner_node = MinerNode(100, 500, 0.1)
-sender = UserNode()
-recipient = UserNode()
+# 生成节点
+users, clouds, miners = generate_nodes(20, 4, 10, 2)
 
-# 发起交易
-sender.send_transaction(recipient, 100, 10)
-transaction_pool.append(Transaction(100, 2000, 10))
+# 生成交易
+transaction_pool = generate_transactions(users)
 
-# 云节点打包
-cloud_node.pack(transaction_pool, miner_node)
+# 打包和挖矿
+pack_and_mine(clouds, miners, transaction_pool)
 
 # 打印节点信息
-print(cloud_node)
-print(miner_node)
-print(sender)
-print(recipient)
+for user in users:
+    print(user)
+for cloud in clouds:
+    print(cloud)
+for miner in miners:
+    print(miner)
+
+
+def main1():
+    # 模拟交易池
+    transaction_pool = []
+
+    # 创建节点
+    cloud_node = CloudNode()
+    miner_node = MinerNode(100, 500, 0.1)
+    sender = UserNode()
+    recipient = UserNode()
+
+    # 发起交易
+    sender.send_transaction(recipient, 100, 10)
+    transaction_pool.append(Transaction(100, 2000, 10))
+
+    # 云节点打包
+    cloud_node.pack(transaction_pool, miner_node)
+
+    # 打印节点信息
+    print(cloud_node)
+    print(miner_node)
+    print(sender)
+    print(recipient)
